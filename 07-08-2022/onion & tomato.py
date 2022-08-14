54b9a77f70344be894924b53b2f19f58c34c38b3
Aravinth-S-731 <aravinth7871867225@gmail.com>
2022-08-14 20:41:27 +0530

    07-08-2022

---
+++ 07-08-2022/onion & tomato.py
@@ -0,0 +1,17 @@
+#Getting amount from user
+rup = int(input("ENTER THE AMOUNT : "))
+print("YOU HAVE RUPEES : ",rup)
+
+#assigning the price of onioin & tomato
+price_onion = 20
+price_tomato = 10.5
+print("Price of 1kg Onion is ",price_onion," & Price of 1kg tomato is ",price_tomato)
+
+#finding the number of kilos the customer can buy
+num = rup/2;
+weight_onion = num/price_onion
+weight_tomato = num/price_tomato
+
+#rounding off the float to two decimal points
+weight_tomato = round(weight_tomato,2)
+print("\n YOU CAN BUY ",weight_onion,"Kgs OF ONION & ",weight_tomato,"Kgs OF TOMATO.")
---
+++ 07-08-2022/wedding expense.py
@@ -0,0 +1,32 @@
+#cost of lunch per person
+lunch = int(input("ENTER THE COST OF LUNCH PER PERSON : "))
+count = int(input("ENTER TOTAL NUMBER OF GUESTS : "))
+print("COST OF LUNCH PER PERSON IS : ",lunch)
+print("TOTAL GUESTS COUNT IS : ",count)
+
+#cost of breakfast
+breakfast = lunch / 2
+print("THE COST OF BREAKFAST IS : ",breakfast)
+
+#cost of hall
+hall =  (1/3) * lunch
+hall = round(hall,2)
+print("THE COST OF HALL IS : ",hall)
+
+#cost of decoration
+decor = (1/2) * hall
+decor = round(decor,2)
+print("THE COST OF DECORATION IS : ",decor)
+
+#cost of parking
+park = (1/10) * hall
+park = round(park,2)
+print("THE COST OF PARKING IS : ",park)
+
+#total expense
+tot_per_head = lunch + breakfast + hall + decor + park
+tot_expense = tot_per_head * count
+print("----------------------------------")
+print("\n THE TOTAL EXPENSE PER HEAD IS : ",tot_per_head,"Rupees")
+print("----------------------------------")
+print("THE TOTAL EXPENSE FOR ",count," PEOPLE IS : ",tot_expense,"Rupees")
