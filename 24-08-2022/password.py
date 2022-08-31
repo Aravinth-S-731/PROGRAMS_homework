f9247f102f07c8db896ae14949ddc11c634392d2
Aravinth-S-731 <aravinth7871867225@gmail.com>
2022-08-31 19:55:59 +0530

    24-08-2022

--- 24-08-2022/password.py
+++ 24-08-2022/password.py
@@ -6,25 +6,25 @@ numbers = "0123456789"
 special_char = "!@#$%^&*()_+-=[],.<>/?\|~`':;"
 count_alpha , count_num , count_spec = 0 , 0 , 0
 password = ""   #to store the password
-while(len(password) < 8):   #to get password until it 8 or more letters long
+while(len(password) < 8):   #to get password until therer is 8 or more letters
     password = input("ENTER YOUR PASSWORD WITH MINIMUM OF 8-CHARACTERS : ")
 print("YOUR PASSWORD IS : ",password)
 password_length = len(password)
-if(password_length >= 8):
+if(password_length >= 8):   #only if the password length is greater or equal to 8, it shuld check the password
     for i in password:
-        if(i in alphabets):
+        if(i in alphabets):     #for every alphabet increase the count of aphabet
             count_alpha += 1
-        if(i in numbers):
+        if(i in numbers):       #for every number increase the count of alphabet
             count_num += 1
-        if(i in special_char):
+        if(i in special_char):  #for every special character increase the count of special character
             count_spec +=1
-if (password_length >= 16) and (count_alpha >= 3) and (count_num>= 2) and (count_spec >= 1):
+if (password_length >= 16) and (count_alpha >= 3) and (count_num>= 2) and (count_spec >= 1):    #to check very strong pasword
     password_strength = "VERY STRONG"
-elif (count_alpha >= 3) and (count_num>= 2) and (count_spec >= 1):
+elif (count_alpha >= 3) and (count_num>= 2) and (count_spec >= 1):  #to check strong password
     password_strength = "STRONG"
-elif (count_alpha >= 1) and (count_num>= 1) and (count_spec >= 1):
+elif (count_alpha >= 1) and (count_num>= 1) and (count_spec >= 1):  #to check ok password
     password_strength = "OK"
-elif (count_alpha == password_length) or (count_num == password_length) or (count_spec == password_length):
+else:                                                               #to check the weak password
     password_strength = "WEAK"
 print(password,"is",password_strength)
 """
